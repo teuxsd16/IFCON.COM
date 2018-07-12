ee1fa0129f089a0ce929609376c3c005a83d7fc8
teuxsd16 <31145959+teuxsd16@users.noreply.github.com>
2018-07-12 17:23:34 -0300

    jkj
    jkj

--- templates/index.php
+++ templates/index.php
@@ -2,55 +2,89 @@
 <html lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>IFCON</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Parallax Template - Materialize</title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300" rel="stylesheet">
-  <link href="../css/materialize.css" type="text/css" rel="stylesheet" />
-  <link href="../css/style.css" type="text/css" rel="stylesheet"/>
+  <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
-  <div class="navbar-fixed">
-      <nav class="" role="navigation" style="background-color:black;">
-        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="#">Inscrições</a></li>
-          </ul>
+<body style="background-color:black;">
+  <nav style="background-color:black; background-size:cover;" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">IF.Con</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
 
-          <ul id="nav-mobile" class="sidenav">
-            <li><a href="#">Navbar Link</a></li>
-          </ul>
-          <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        </div>
-      </nav>
-</div>
-
-
-
-<!-- Tap Target Structure -->
-
- <div class="tap-target" data-target="menu">
-   <div class="tap-target-content">
-     <h5>Title</h5>
-     <p>A bunch of text</p>
-   </div>
- </div>
-
-
-  <div class="carousel carousel-slider" style="height:500px;">
-      <a class="carousel-item" href="#one!"><img src="../img/a1.jpg"></a>
-      <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"></a>
-      <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"></a>
-      <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"></a>
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+
+    <div class="parallax"><img src="../img/a5.jpg" height="400px" alt="Unsplashed background img 1"></div>
+  </div>
 
 
+  <div class="container" style="color:white;">
+    <div class="section">
 
-<!-- Contato -->
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center white-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Speeds up development</h5>
 
-<div style="background-color:#01579b;; width:100%; padding:10%;">
+            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center white-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">User Experience Focused</h5>
+
+            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center white-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Easy to work with</h5>
+
+            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="../img/a7.jpg" width="1000px" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div style=" width:100%; padding:10%;color:white;" class="black">
+  <h2 class="font" style="margin-top:-5%;" align="center">Sobre</h2>
+  <p style="font-size: 20pt;"> Tendo sua primeira edição realizada em 2016 o IF.Con é um evento que ocorre inspirado na COMIC CON (evento de cultura pop/nerd de origem norte americana) e que busca discutir a cultura pop em seus diversos aspectos e formas (cinema, literatura, quadrinhos, desenhos, pinturas, animes, games, etc.) de maneira mais acadêmica como estratégia lúdica de educação.</p>
+  <br><br><br>
+  <h2 class="font"  align="center">Programação</h2>
+  <p style="font-size: 20pt;"> Tendo sua primeira edição realizada em 2016 o IF.Con é um evento que ocorre inspirado na COMIC CON (evento de cultura pop/nerd de origem norte americana) e que busca discutir a cultura pop em seus diversos aspectos e formas (cinema, literatura, quadrinhos, desenhos, pinturas, animes, games, etc.) de maneira mais acadêmica como estratégia lúdica de educação.</p>
   <!-- Galeria -->
   <h2 class="font" style="margin-top:-3%;" align="center">Galeria</h2>
   <br>
@@ -98,57 +132,51 @@
 </div>
 
 
-<footer class="page-footer black">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Company Bio</h5>
-        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
-      </div>
-      <div class="col l3 s12">
-        <h5 class="white-text">Settings</h5>
-        <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
-        </ul>
-      </div>
-      <div class="col l3 s12">
-        <h5 class="white-text">Connect</h5>
-        <ul>
-          <li><a class="white-text" href="#!">Link 1</a></li>
-          <li><a class="white-text" href="#!">Link 2</a></li>
-          <li><a class="white-text" href="#!">Link 3</a></li>
-          <li><a class="white-text" href="#!">Link 4</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-
-  <div class="footer-copyright">
+  <footer class="page-footer " style="background-color:white; color:black;">
     <div class="container">
-    Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</footer>
-
-
-
-
-
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
 
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="../js/materialize.js"></script>
   <script>
-
-    M.AutoInit();
-
+      M.AutoInit();
   </script>
+
+
 
   </body>
 </html>

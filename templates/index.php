671b78cd94e868a51f5f6afe1dbedad852a6f389
teuxsd16 <31145959+teuxsd16@users.noreply.github.com>
2018-07-16 14:40:36 -0300

    hghg
    hghghggh

--- templates/index.php
+++ templates/index.php
@@ -39,7 +39,7 @@
       <div class="row">
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center white-text"><i class="material-icons">flash_on</i></h2>
+            <h2 class="center white-text"><i class="material-icons">record_voice_over</i></h2>
             <h5 class="center">Games</h5>
 
             <p class="light" style="font-family: 'Nunito', sans-serif;">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
@@ -48,7 +48,7 @@
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center white-text"><i class="material-icons">group</i></h2>
+            <h2 class="center white-text"><i class="material-icons">face</i></h2>
             <h5 class="center">User Experience Focused</h5>
 
             <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
@@ -57,7 +57,7 @@
 
         <div class="col s12 m4">
           <div class="icon-block">
-            <h2 class="center white-text"><i class="material-icons">settings</i></h2>
+            <h2 class="center white-text"><i class="material-icons">games</i></h2>
             <h5 class="center">Easy to work with</h5>
 
             <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
